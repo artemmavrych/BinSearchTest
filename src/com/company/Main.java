0f707eac22823e203f55d7771200049ef82e79dc
artem <artem@mavrych>
2021-08-26 17:27:17 +0300

    Bad Binary search

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,38 @@
 package com.company;
-
+import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-
+            Integer[] array={5,9,13,18,20,35,80};
+            System.out.println(ArrBinSearch.BinSearch(array,18));
     }
 }
 
 class ArrBinSearch{
-    public static <T extends Number> T BinSearch(T[] inputArray){
+    public static <T extends Number> int BinSearch(T[] inputArray, T searchNubmer) {
+        return Binary(inputArray, searchNubmer,0, inputArray.length-1 );
+        }
+    private static<T extends Number> int Binary(T[] inputArray, T searchNubmer,int start, int end ){
+        int startIndex=start;
+        int endIndex=end;
+        int midIndex = (endIndex+startIndex) / 2;
+        if(endIndex==0) return -1;
+        if (inputArray[midIndex].doubleValue() > searchNubmer.doubleValue()) {
+            endIndex = midIndex;
+            return Binary(inputArray, searchNubmer, startIndex, endIndex );
 
-        return T;
+        } else if (inputArray[midIndex].doubleValue() < searchNubmer.doubleValue()) {
+            startIndex = midIndex;
+            if(startIndex==endIndex) return -1;
+            if((endIndex-startIndex)==1) return Binary(inputArray, searchNubmer, endIndex, endIndex);
+            return Binary(inputArray, searchNubmer, startIndex, endIndex);
+        }
+        else if(inputArray[midIndex].doubleValue() == searchNubmer.doubleValue()){
+            return midIndex;
+        }
+        //else
+        else return -1;
     }
-
 
 
 }
